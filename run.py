438b4fe920b2c1e71df7c204bf2b9c9e22acd9ac
thunderbolt181 <baghel.sachin47@gmail.com>
2022-06-22 21:49:54 +0530

    adding readme

--- run.py
+++ run.py
@@ -2,6 +2,17 @@ from multiprocessing import Process,Pipe
 from bot.bot import run
 from pub_sub.pub_sub import PubSub
 
+"""
+Process 1: Discord bot process
+Process 2: Pub/Sub Process
+
+To stop any process related to discord bot 
+comment out every line related to process1.
+
+To stop any process related to notifications or pub/sub api
+comment out every line related to process2.
+"""
+
 if __name__=="__main__":
     discord_conn, pubsub_conn = Pipe()
     process1 = Process(target=run,args=(discord_conn,))
